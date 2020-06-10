4eea726125a32820962c98675e2d6b869aaaaece
Trofis <porto.thomas@gmail.com>
2020-06-10 11:18:16 +0200

    server config resolved (logstash)

--- server/src/modules/global_vars.js
+++ server/src/modules/global_vars.js
@@ -4,7 +4,7 @@ const loc = require('./locations')
 
 // Global vars used in code
 const global = {
-    logstash_dir: process.env.logstash || "/home/trofis/Workspace/Worldline/autoPairing",
+    logstash_dir: process.env.LOGSTASH || "/home/trofis/Workspace/Worldline/autoPairing",
     logs_dir:"/tmp/logs_modem_files",
     config_logstash_name:'logstash-config.conf',
     logstash_process_pid:undefined,
